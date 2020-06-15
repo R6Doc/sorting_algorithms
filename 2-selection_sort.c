927bd9e5ad0c06932bc6d9bd2a78e110e6702855
R6Doc <fiendgear@gmail.com>
2020-06-15 18:14:46 -0500

    fixed logic

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,6 +8,27 @@
 void selection_sort(int *array, size_t size)
 {
 
+	size_t i, j, min;
+	int swap;
 
-	
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (i != min)
+		{
+			swap = array[i];
+			array[i] = array[min];
+			array[min] = swap;
+			print_array(array, size);
+		}
+	}
+
 }
